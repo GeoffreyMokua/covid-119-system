f0e7311509914f05147423adb8d0b50369ac2d9f
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-19 21:49:35 +0300

    fetching data from countries API

--- src/components/FilterSearch.js
+++ src/components/FilterSearch.js
@@ -10,4 +10,34 @@ function FilterSearch({ searchFilter, searchCountry }) {
     
     React.useEffect(() => {
 		getCountries();
-	}, []);
+    }, []);
+    
+    const getCountries = () => {
+		const options = {
+			method: "GET",
+			headers: {
+				"X-RapidAPI-Key": "ae9e57d040msh01d6f2dab70ab6dp1afefcjsnddbe7e9267cc",
+				"X-RapidAPI-Host": "covid-193.p.rapidapi.com",
+			},
+		};
+
+		fetch("https://covid-193.p.rapidapi.com/countries", options)
+			.then((res) => res.json())
+			.then((res) => setCountries(res.response))
+			.catch((err) => console.error(err));
+	};
+	// console.log(countries);
+	// Implimenting all the options from the fetched data
+	const handleChange = (e) => {
+		setSearch(e.target.value);
+	};
+	const countryOptions = countries.map((country, index) => {
+		const options = {};
+		options.label = country;
+		options.id = index;
+		return options;
+	});
+
+	searchCountry(search);
+
+	// console.log("Country Options ==>", countryOptions);
