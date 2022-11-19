687ec7dc6d63f5e2fffef7744124214b5b3e511d
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-19 21:47:59 +0300

    setting the useEffect hook in performing DOM upadtes

--- src/components/FilterSearch.js
+++ src/components/FilterSearch.js
@@ -6,4 +6,8 @@ import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
 
 function FilterSearch({ searchFilter, searchCountry }) {
 	const [countries, setCountries] = React.useState([]);
-	const [search, setSearch] = React.useState("");
+    const [search, setSearch] = React.useState("");
+    
+    React.useEffect(() => {
+		getCountries();
+	}, []);
