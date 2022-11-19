43232c365ff55a61cc66b7d2841e887e7e9be3b4
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-19 21:45:22 +0300

    setting state in search filter

---
+++ src/components/FilterSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../styles/FilterSearch.css";
+import Select from "./Select";
+import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
+
+
+function FilterSearch({ searchFilter, searchCountry }) {
+	const [countries, setCountries] = React.useState([]);
+	const [search, setSearch] = React.useState("");
